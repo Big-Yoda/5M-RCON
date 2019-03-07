ec5b850de2337d615787b0add365ba8c5bd4563a
viggo95yoda <43821187+viggo95yoda@users.noreply.github.com>
2019-03-07 19:57:17 +0100

    Create set.js

---
+++ cmds/set.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const botconfig = require('../botconfig.json');
+const fs = require('fs');
+const request = require('request');
+
+module.exports = class set {
+    constructor()
+    {
+        this.name = 'set',
+        this.prefix = '->',
+        this.alias = ['setrcon'],
+        this.usage = '->set IP:PORT'
+    }
+
+    async run(bot, message, args)
+    {
+        message.delete();
+        if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`:x: Insufficient Permission!`);
+        let cfg = JSON.parse(fs.readFileSync(`./configs/config.json`, "utf8"));
+        let ipandport = args[1].split(":");
+        let pswd;
+        if (!ipandport[1]) return message.channel.send(`:x: Please specify IP, Port & Rcon Password. Ex. ${this.usage}`);
+        message.author.send(`Rcon password:`).then(msg => {
+            const filter = m => m.author.id === message.author.id;
+            msg.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(daMessage => {
+                if (!cfg[message.guild.id])
+                {
+                    cfg[message.guild.id] = {
+                        ip: ipandport[0],
+                        port: ipandport[1],
+                        password: daMessage
+                    }
+                }
+                else
+                {
+                    cfg[message.guild.id].ip = ipandport[0];
+                    cfg[message.guild.id].port = ipandport[1];
+                    cfg[message.guild.id].password = daMessage;
+                }
+                msg.channel.send(`:white_check_mark: Rcon password set!`);
+            });
+        });
+        fs.writeFileSync(`./configs/config.json`, JSON.stringify(cfg, null, 4));
+            let setEmbed = new Discord.RichEmbed()
+                .setTitle(`FiveM RCON`)
+                .setDescription(`You set **"${message.guild.name}"** Rcon Info!`)
+                .setFooter(`IP & Port: ${ipandport[0]}:${ipandport[1]}`);
+            message.channel.send(setEmbed);
+        return;
+    }
+}
