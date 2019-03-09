ae954bb8c3bf5dfab6dc1f3e04433b2380e52a01
viggo95yoda <43821187+viggo95yoda@users.noreply.github.com>
2019-03-09 13:26:43 +0100

    Update set.js

--- cmds/set.js
+++ cmds/set.js
@@ -14,7 +14,6 @@ module.exports = class set {
 
     async run(bot, message, args)
     {
-        message.delete();
         if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`:x: Insufficient Permission!`);
         let cfg = JSON.parse(fs.readFileSync(`./configs/config.json`, "utf8"));
         let ipandport = args[1].split(":");
@@ -22,25 +21,27 @@ module.exports = class set {
         if (!ipandport[1]) return message.channel.send(`:x: Please specify IP, Port & Rcon Password. Ex. ${this.usage}`);
         message.author.send(`Rcon password:`).then(msg => {
             const filter = m => m.author.id === message.author.id;
-            msg.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(daMessage => {
+            msg.channel.awaitMessages(filter, { max: 1, time: 100000 }).then(daMessage => {
+				console.log(daMessage.first().content);
                 if (!cfg[message.guild.id])
                 {
                     cfg[message.guild.id] = {
                         ip: ipandport[0],
                         port: ipandport[1],
-                        password: daMessage
+                        password: daMessage.first().content
                     }
                 }
                 else
                 {
                     cfg[message.guild.id].ip = ipandport[0];
                     cfg[message.guild.id].port = ipandport[1];
-                    cfg[message.guild.id].password = daMessage;
+                    cfg[message.guild.id].password = daMessage.first().content;
                 }
                 msg.channel.send(`:white_check_mark: Rcon password set!`);
-            });
+                fs.writeFileSync(`./configs/config.json`, JSON.stringify(cfg, null, 4));
+			});
         });
-        fs.writeFileSync(`./configs/config.json`, JSON.stringify(cfg, null, 4));
+		console.log(cfg);
             let setEmbed = new Discord.RichEmbed()
                 .setTitle(`FiveM RCON`)
                 .setDescription(`You set **"${message.guild.name}"** Rcon Info!`)
